1018f359ecc5616a233eb2a56b29d19a75fa1a62
Paul Moya <paul.moya@enti.cat>
2018-10-25 20:29:56 +0200

    Modified Script and Canvas
    Modified the UIWorldPosition and added the canvas for the UI elements

---
+++ Assets/Scripts/Utilities/UIWorldPosition.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIWorldPosition : MonoBehaviour {
+    public Transform target;                                                    // Game Object to which the UI element will be attached 
+    public Camera mainCamera;                                                   // Camera where the UI will be displayed
+
+    private Vector3 realPosition;                                               // Variable that saves the real position in the world where the UI element will be displayed
+
+    void Start() {
+        realPosition = target.position + new Vector3(0, 1.1f, 0);               // Calculates the real position
+    }
+
+    void Update() {
+        Vector3 screenPosition = mainCamera.WorldToScreenPoint(realPosition);   // Calculates the equivalent of screen position to the real position of the UI element
+        transform.position = screenPosition;                                    // Updates the UI element in the screen position
+    }
+}
