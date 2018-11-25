26a84123acd5770237ef18cd7ae5a1c92a5db807
Juan Lojo Callón <juan.lojo@enti.cat>
2018-11-25 19:35:11 +0100

    Added initial animations
    Addes animations to almost all of the attack options in the combat. Also a walking animation that needs some work when changing the orientation of the character.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,9 +15,13 @@ public class PlayerController : MonoBehaviour {
     private int X_MOVEMENT;                                             // Variable that saves the number that multiplies the control to invert it
     private int Z_MOVEMENT;                                             // Variable that saves the number that multiplies the control to invert it
 
-    void Start () {                                        
-        transform.position = new Vector3(0, 1, 0);                      // Positions in which the character will be spawned
-        transform.eulerAngles = new Vector3(0, 0, 0);                   // Orientation of character spawn
+    Animator animation;                                                 // It saves the animator.
+
+    void Start () {
+        animation = GetComponent<Animator>();                           // Gets the animator from the actual character.
+
+        transform.position = new Vector3(0, 0.48f, 0);                      // Positions in which the character will be spawned
+        transform.eulerAngles = new Vector3(0, 90, 0);                   // Orientation of character spawn
           
         mainCameraTransform = mainCamera.GetComponent<Transform>();     // Gets the main camera transform information
         mainCameraTransform.position    = new Vector3(-5, 3, 0);        // Position where the camera is set at start
@@ -28,19 +32,45 @@ public class PlayerController : MonoBehaviour {
         if (xMovementInverted) X_MOVEMENT = -1;                         // Sets the numbers to inverted or not movement in x axis
         else X_MOVEMENT = 1;
 
-        if (zMovementInverted) Z_MOVEMENT =  1;                         // Sets the numbers to inverted or not movement in x axis
-        else Z_MOVEMENT = -1;
+        if (zMovementInverted) Z_MOVEMENT =  -1;                         // Sets the numbers to inverted or not movement in x axis
+        else Z_MOVEMENT = 1;
 
         speed = 5.0f;                                                   // Sets the speed number
     }
 	
 	void Update () {
-        float xMove = Input.GetAxis("Vertical")   * speed * X_MOVEMENT; // Gets the input of the vertical axis and applies speed 
-        float zMove = Input.GetAxis("Horizontal") * speed * Z_MOVEMENT; // Gets the input of the horizontal axis and applies speed 
+        float xMove = Input.GetAxis("Horizontal")   * speed * X_MOVEMENT; // Gets the input of the vertical axis and applies speed 
+        float zMove = Input.GetAxis("Vertical") * speed * Z_MOVEMENT; // Gets the input of the horizontal axis and applies speed 
 
         xMove *= Time.deltaTime;                                        // Makes the movement based on time and not in frames per second
         zMove *= Time.deltaTime;                                        // Makes the movement based on time and not in frames per second
-            
+
+        if(xMove > 0 || xMove < 0 || zMove > 0 || zMove < 0)            // Sets a an animation parameter based on how it is moving
+        {
+            animation.SetFloat("Speed", 1);                        
+        }
+        else if(xMove == 0 || zMove == 0)
+        {
+            animation.SetFloat("Speed", 0);
+        }
+
+        //if (xMove < 0)                                                   // Changes the rotation of the character based on the direction it is moving
+        //{
+        //    transform.eulerAngles = new Vector3(0, 0, 0);
+        //}
+        //else if (xMove > 0)
+        //{
+        //    transform.eulerAngles = new Vector3(0, 180, 0);
+        //}
+        //else if (zMove < 0)
+        //{
+        //    transform.eulerAngles = new Vector3(0, 270, 0);
+        //}
+        //else if (zMove > 0)
+        //{
+        //    transform.eulerAngles = new Vector3(0, 90, 0);
+        //}
+
         transform.Translate(xMove, 0, zMove);                           // Transforms the player position
         mainCamera.transform.position = transform.position + offset;    // Transforms the camera position
 	}
