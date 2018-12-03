4628d3dc078d5d2eec4b79093b52f6d369984518
Mario Camara Ramil <mario.camara@enti.cat>
2018-12-03 18:02:53 +0100

    Added Basic Splash Screen and Scenario 1

---
+++ Assets/Scripts/Utilities/JumpScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class JumpScene : MonoBehaviour {
+    public string SceneToLoad = "Lobby";
+
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(3.0f);
+        SceneManager.LoadScene(SceneToLoad, LoadSceneMode.Single);
+    }
+}
