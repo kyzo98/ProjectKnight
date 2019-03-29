f2dc6ce1a1438751dd14022b978b3ce4168cf4b3
Paul Moya <paul.moya@enti.cat>
2019-03-29 21:47:34 +0100

    Added Base Cinematic Scene
    Added the cinematic scene and the base particle systems for the player teleportation

---
+++ Assets/Scripts/Cinematics/CinematicPlayer.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CinematicPlayer : MonoBehaviour {
+
+    
+    public GameObject player;
+    public GameObject teleportAura;
+    public GameObject teleportOrb;
+
+
+    void Start()
+    {
+
+        player.SetActive(false);
+        teleportAura.SetActive(false);
+        teleportOrb.SetActive(false);
+
+    }
+
+
+    void Update()
+    {
+        StartCoroutine(AuraWaiter());
+        StartCoroutine(OrbWaiter());
+        StartCoroutine(PlayerWaiter()); 
+        
+    }
+
+    IEnumerator AuraWaiter()
+    {
+        yield return new WaitForSecondsRealtime(2);
+        teleportAura.SetActive(true);
+        yield return new WaitForSecondsRealtime(8.3f);
+        teleportAura.SetActive(false);
+        // yield return new WaitForSecondsRealtime(0.5f);
+        DestroyTeleportAura(); 
+    }
+    IEnumerator OrbWaiter()
+    {
+        yield return new WaitForSecondsRealtime(2);
+        teleportOrb.SetActive(true);
+      
+        yield return new WaitForSecondsRealtime(8.5f);
+        teleportOrb.SetActive(false);
+        yield return new WaitForSecondsRealtime(0.3f);
+        DestroyTeleportOrb(); 
+        
+    }
+
+    IEnumerator PlayerWaiter()
+    {
+        yield return new WaitForSecondsRealtime(7);
+        player.SetActive(true); 
+    }
+
+    void DestroyTeleportAura()
+    {
+        Destroy(teleportAura);
+    }
+
+    void DestroyTeleportOrb()
+    {
+        Destroy(teleportOrb);
+    }
+
+
+}
