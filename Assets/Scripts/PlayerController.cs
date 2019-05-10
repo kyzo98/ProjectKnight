87f8780c0e97bb1ac736ce478aa52c91f87f923d
Unknown <juan.lojo@enti.cat>
2019-05-11 00:57:10 +0100

    Lobby movement fixed :)
    It has a new camera.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-    public Camera mainCamera;                                           // Camera that follows the player in the lobby
+    private Camera mainCamera;                                           // Camera that follows the player in the lobby
     public bool xMovementInverted;                                      // Bool to on/off inverted movement in x axis
     public bool zMovementInverted;                                      // Bool to on/off inverted movement in z axis
 
@@ -12,6 +12,7 @@ public class PlayerController : MonoBehaviour {
     private Transform mainCameraTransform;                              // Variable that saves the main camera transform
     private int X_MOVEMENT;                                             // Variable that saves the number that multiplies the control to invert it
     private int Z_MOVEMENT;                                             // Variable that saves the number that multiplies the control to invert it
+    private Rigidbody rigidbody;
 
     public Animator anim;                                                      // It saves the animator.
 
@@ -22,18 +23,16 @@ public class PlayerController : MonoBehaviour {
 
     void Start () {
         anim = this.GetComponent<Animator>();                                // Gets the animator from the actual character.
+        rigidbody = this.GetComponent<Rigidbody>();
+        mainCamera = Camera.main;
 
         transform.position = new Vector3(4.89f, 0.002f, 4.56f);                      // Positions in which the character will be spawned
         transform.eulerAngles = new Vector3(0, 0, 0);                   // Orientation of character spawn
-          
-        mainCameraTransform = mainCamera.GetComponent<Transform>();     // Gets the main camera transform information
-        mainCameraTransform.position    = new Vector3(-0.43f, 3f, 4.64f);        // Position where the camera is set at start
-        mainCameraTransform.eulerAngles = new Vector3(30, 90, 0);       // Orientation of camera spawn
 
         offset = mainCamera.transform.position - transform.position;    // Measures the distance between the character and the camera
 
-        if (xMovementInverted) X_MOVEMENT = 1;                         // Sets the numbers to inverted or not movement in x axis
-        else X_MOVEMENT = -1;
+        if (xMovementInverted) X_MOVEMENT = -1;                         // Sets the numbers to inverted or not movement in x axis
+        else X_MOVEMENT = 1;
 
         if (zMovementInverted) Z_MOVEMENT =  -1;                         // Sets the numbers to inverted or not movement in x axis
         else Z_MOVEMENT = 1;
@@ -110,11 +109,12 @@ public class PlayerController : MonoBehaviour {
             anim.SetFloat("Speed", 0);
         }
 
-        Vector3 orientation = new Vector3(zMove, 0, xMove);                                               // Saves the orientation of the player
+        Vector3 orientation = mainCamera.transform.forward * zMove + mainCamera.transform.right * xMove;                                               // Saves the orientation of the player based in where the camera is looking
         transform.rotation = Quaternion.LookRotation(orientation);                                        // Changes the orientation of the player based in his movement direction
         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(orientation), 0.15f);  // Makes the orientation change more smooth.
 
-        transform.Translate(orientation * speed * Time.deltaTime, Space.World);                           // Transforms the player position
-        mainCamera.transform.position = transform.position + offset;                                      // Transforms the camera position
+        rigidbody.velocity = orientation * speed;                                                           //Moves the player. Changed it because the transform.translate doesn't care about all the collisions
+
+        //We don't need to update camera position now, is a free look cinemachine now and using it's follow and look at components is all we need to update it.
 	}
 }
